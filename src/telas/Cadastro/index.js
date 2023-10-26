691d8d77b91522ece5902c5e2984ace5f5808566
Antony Maziero Warken <againplayi7@gmail.com>
2023-10-26 14:26:06 -0300

    cap 2 finalizado

--- src/telas/Cadastro/index.js
+++ src/telas/Cadastro/index.js
@@ -1,25 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, View } from 'react-native';
 import Botao from '../../componentes/Botao';
 import { EntradaTexto } from '../../componentes/EntradaTexto';
 import estilos from './estilos';
-import { auth } from '../../config/firebase';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { cadastrar } from '../../servicos/requisicoesFirebase';
+import { Alerta } from '../../componentes/Alerta';
+
 
 export default function Cadastro({ navigation }) {  
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [confirmaSenha, setConfirmaSenha] = useState('');
+  const [statusError, setStatusError] = useState('');
+  const [mensagemError, setMensagemError] = useState('');
 
-  useEffect(() => {
-    createUserWithEmailAndPassword(auth, "andre2@email.com", "123456")
-    .then((dadosDoUsuario) => {
-      console.log(dadosDoUsuario)
-    })
-    .catch((error) => {
-      console.log(error)
-    });
-  }, [])
+  async function realizarCadastro(){
+    if(email == ''){
+      setMensagemError('Preencha com seu email');
+      setStatusError('email');
+    } else if(senha == ''){
+      setMensagemError('Digite sua senha');
+      setStatusError('senha');
+    } else if(confirmaSenha == ''){
+      setMensagemError('Confirme sua senha');
+      setStatusError('confirmaSenha');
+    } else if(confirmaSenha != senha){
+      setMensagemError('As senhas não conferem!');
+      setStatusError('confirmaSenha');
+    } else {
+      const resultado = await cadastrar(email, senha);
+      setStatusError('firebase')
+      if(resultado == 'sucesso') {
+        setMensagemError('Usuário criado com sucesso!')
+        setEmail('')
+        setSenha('')
+        setConfirmaSenha('')
+      }
+      else {
+        setMensagemError(resultado)
+      }
+    }
+  }
 
   return (
     <View style={estilos.container}>
@@ -27,12 +48,16 @@ export default function Cadastro({ navigation }) {
         label="E-mail"
         value={email}
         onChangeText={texto => setEmail(texto)}
+        error={statusError == 'email'}
+        messageError={mensagemError}
       />
       <EntradaTexto
         label="Senha"
         value={senha}
         onChangeText={texto => setSenha(texto)}
         secureTextEntry
+        error={statusError == 'senha'}
+        messageError={mensagemError}
       />
 
       <EntradaTexto
@@ -40,9 +65,17 @@ export default function Cadastro({ navigation }) {
         value={confirmaSenha}
         onChangeText={texto => setConfirmaSenha(texto)}
         secureTextEntry
+        error={statusError == 'confirmaSenha'}
+        messageError={mensagemError}
+      />
+
+      <Alerta 
+        mensagem={mensagemError}
+        error={statusError == 'firebase'}
+        setError={setStatusError}
       />
       
-      <Botao onPress={() => {}}>CADASTRAR</Botao>
+      <Botao onPress={() => realizarCadastro()}>CADASTRAR</Botao>
     </View>
   );
 }
